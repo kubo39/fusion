66f1ae4ab5d17555ec91653eca04a645b535c003
Hiroki Noda <kubo39@gmail.com>
2023-12-05 15:25:46 +0900

    Hello, world!

--- source/app.d
+++ source/app.d
@@ -1,9 +1,50 @@
+import ldc.attributes : naked;
+import ldc.llvmasm;
+
 extern (C):
 
 alias EfiStatus = uint;
 alias EfiHandle = void*;
 
-struct EfiSystemTable {}
+struct EfiTableHeader
+{
+	ulong signature;
+	uint revision;
+	uint headerSize;
+	uint crc32;
+	uint reserved;
+}
+
+struct EfiSystemTable
+{
+	EfiTableHeader header;
+	wchar* firmwareVendor;
+	uint firmwareRevision;
+	EfiHandle consoleInHandle;
+	void* conIn;
+	EfiHandle consoleOutHandle;
+	SimpleTextOutput* conOut;
+	EfiHandle standardErrorHandle;
+	void* stdErr;
+	void* runtimeServices;
+	void* bootServices;
+	uint numTableEntries;
+	void* configTable;
+}
+
+struct SimpleTextOutput
+{
+	void* reset;
+	EfiStatus function(SimpleTextOutput*, wchar*) outputString;
+	void* testString;
+	void* queryMode;
+	EfiStatus function(SimpleTextOutput*, uint) setMode;
+	void* setAttribute;
+	EfiStatus function(SimpleTextOutput*) clearScreen;
+	void* setCursorPos;
+	void* enableCursor;
+	void** mode;
+}
 
 enum : EfiStatus
 {
@@ -11,10 +52,24 @@ enum : EfiStatus
 	EfiLoadError = 1
 }
 
+@naked void exit(int status)
+{
+	__asm(`
+	.loop:
+		cli
+		hlt
+		jmp .loop
+	`, "");
+}
+
 void d_main() {}
 
 EfiStatus efi_main(EfiHandle imgHandle, EfiSystemTable* sysTable)
 {
 	d_main();
+	wchar* msg = cast(wchar*) "Hello, World\0"w.ptr;
+	sysTable.conOut.clearScreen(sysTable.conOut);
+	sysTable.conOut.outputString(sysTable.conOut, msg);
+	exit(0);
 	return EfiLoadError;
 }
