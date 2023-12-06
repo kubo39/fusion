6fa9826dc1079c8f3de56430867e8fb5413aaba5
Hiroki Noda <kubo39@gmail.com>
2023-12-06 14:35:18 +0900

    Move some uefi related definition

--- source/app.d
+++ source/app.d
@@ -1,56 +1,9 @@
 import ldc.attributes : naked;
 import ldc.llvmasm;
 
+import uefi;
+
 extern (C):
-
-alias EfiStatus = uint;
-alias EfiHandle = void*;
-
-struct EfiTableHeader
-{
-	ulong signature;
-	uint revision;
-	uint headerSize;
-	uint crc32;
-	uint reserved;
-}
-
-struct EfiSystemTable
-{
-	EfiTableHeader header;
-	wchar* firmwareVendor;
-	uint firmwareRevision;
-	EfiHandle consoleInHandle;
-	void* conIn;
-	EfiHandle consoleOutHandle;
-	SimpleTextOutput* conOut;
-	EfiHandle standardErrorHandle;
-	void* stdErr;
-	void* runtimeServices;
-	void* bootServices;
-	uint numTableEntries;
-	void* configTable;
-}
-
-struct SimpleTextOutput
-{
-	void* reset;
-	EfiStatus function(SimpleTextOutput*, wchar*) outputString;
-	void* testString;
-	void* queryMode;
-	EfiStatus function(SimpleTextOutput*, uint) setMode;
-	void* setAttribute;
-	EfiStatus function(SimpleTextOutput*) clearScreen;
-	void* setCursorPos;
-	void* enableCursor;
-	void** mode;
-}
-
-enum : EfiStatus
-{
-	EfiSuccess = 0,
-	EfiLoadError = 1
-}
 
 @naked void exit(int status)
 {
