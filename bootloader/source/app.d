f0aba1b2a199766b3839616cea8dd1d57d1fdd21
Hiroki Noda <kubo39@gmail.com>
2025-01-08 00:10:06 +0900

    Loading the Kernel (Part 1)

--- bootloader/source/app.d
+++ bootloader/source/app.d
@@ -15,10 +15,55 @@ extern (C):
     `, "");
 }
 
-void EfiMainInner(EfiSystemTable* sysTable)
+void checkStatus(EfiStatus status)
+{
+    if (status != EfiSuccess)
+    {
+        consoleOut(cast(wchar*) " [failed]"w.ptr);
+        exit(0);
+    }
+    consoleOut(cast(wchar*) " [success]\r\n"w.ptr);
+}
+
+void EfiMainInner(EfiHandle imgHandle, EfiSystemTable* sysTable)
 {
     uefi.sysTable = sysTable;
     consoleClear();
+
+    consoleOut(cast(wchar*) "Fusion OS Bootloader\r\n");
+
+    EfiStatus status;
+    EfiLoadedImageProtocol* loadedImage;
+
+    consoleOut(cast(wchar*) "boot: Acquiring LoadedImage protocol"w.ptr);
+    checkStatus(uefi.sysTable.bootServices.handleProtocol(
+        imgHandle, EfiLoadedImageProtocolGuid, &loadedImage
+    ));
+
+    EfiSimpleFileSystemProtocol* fileSystem;
+    consoleOut(cast(wchar*) "boot: Acquiring SimpleFileSystem protocol"w.ptr);
+    checkStatus(uefi.sysTable.bootServices.handleProtocol(
+        loadedImage.deviceHandle, EfiSimpleFileSystemProtocolGuid, &fileSystem
+    ));
+
+    EfiFileProtocol* rootDir;
+    consoleOut(cast(wchar*) "boot: Opening directory"w.ptr);
+    checkStatus(fileSystem.openVolume(fileSystem, &rootDir));
+
+    // open the kernel file
+    EfiFileProtocol* kernelFile;
+    auto kernelPath = cast(wchar*) r"efi\fusion\kernel.bin"w.ptr;
+
+    consoleOut(cast(wchar*) "boot: Opening kernel file: "w.ptr);
+    consoleOut(kernelPath);
+    checkStatus(rootDir.open(rootDir, &kernelFile, kernelPath, 1, 1));
+
+    // get kernel file size
+    EfiFileInfo kernelInfo = void;
+    uint kernelInfoSize = EfiFileInfo.sizeof;
+
+    consoleOut(cast(wchar*) "boot: Getting kernel file info"w.ptr);
+    checkStatus(kernelFile.getInfo(kernelFile, &EfiFileInfoGuid, &kernelInfoSize, &kernelInfo));
 }
 
 void Dmain() {}
@@ -27,12 +72,10 @@ EfiStatus efi_main(EfiHandle imgHandle, EfiSystemTable* sysTable)
 {
     Dmain();
 
-    EfiMainInner(sysTable);
+    EfiMainInner(imgHandle, sysTable);
 
     // String literals have a '\0' appended.
     // https://dlang.org/spec/expression.html#string_literals
-    wchar* msg = cast(wchar*) "Hello, World!"w.ptr;
-    consoleOut(msg);
     exit(0);
     return EfiLoadError;
 }
--- bootloader/source/uefi.d
+++ bootloader/source/uefi.d
@@ -38,7 +38,7 @@ struct EfiSystemTable
     EfiHandle standardErrorHandle;
     void* stdErr;
     void* runtimeServices;
-    void* bootServices;
+    EfiBootServices* bootServices;
     uint numTableEntries;
     void* configTable;
 }
@@ -62,3 +62,186 @@ enum : EfiStatus
     EfiSuccess = 0,
     EfiLoadError = 1
 }
+
+struct EfiLoadedImageProtocol
+{
+    uint revision;
+    EfiHandle parentHandle;
+    EfiSystemTable* systemTable;
+    // Source location of the image
+    EfiHandle deviceHandle;
+    void* filePath;
+    void* reserved;
+    // Image's load options
+    uint loadOptionsSize;
+    void* loadOptions;
+    // Location where image was loaded
+    void* imageBase;
+    ulong imageSize;
+    EfiMemoryType imageCodeType;
+    EfiMemoryType imageDataType;
+    void* unload;
+}
+
+ enum EfiMemoryType
+ {
+    EfiReservedMemory,
+    EfiLoaderCode,
+    EfiLoaderData,
+    EfiBootServicesCode,
+    EfiBootServicesData,
+    EfiRuntimeServicesCode,
+    EfiRuntimeServicesData,
+    EfiConventionalMemory,
+    EfiUnusableMemory,
+    EfiACPIReclaimMemory,
+    EfiACPIMemoryNVS,
+    EfiMemoryMappedIO,
+    EfiMemoryMappedIOPortSpace,
+    EfiPalCode,
+    EfiPersistentMemory,
+    EfiUnacceptedMemory,
+    OsvKernelCode = 0x80000000,
+    OsvKernelData = 0x80000001,
+    OsvKernelStack = 0x80000002,
+    EfiMaxMemoryType,
+ }
+
+struct EfiBootServices
+{
+    EfiTableHeader hdr;
+    // task priority services
+    void* raiseTpl;
+    void* restoreTpl;
+    // memory services
+    void* allocatePages;
+    void* freePages;
+    void* getMemoryMap;
+    void* allocatePool;
+    void* freePool;
+    // event & timer services
+    void* createEvent;
+    void* setTimer;
+    void* waitForEvent;
+    void* signalEvent;
+    void* closeEvent;
+    void* checkEvent;
+    // protocol handler services
+    void* installProtocolInterface;
+    void* reinstallProtocolInterface;
+    void* uninstallProtocolInterface;
+    EfiStatus function(EfiHandle, EfiGuid, void*) handleProtocol;
+    void* reserved;
+    void* registerProtocolNotify;
+    void* locateHandle;
+    void* locateDevicePath;
+    void* installConfigurationTable;
+    // image services
+    void* loadImage;
+    void* startImage;
+    void* exit;
+    void* unloadImage;
+    void* exitBootServices;
+    // misc services
+    void* getNextMonotonicCount;
+    void* stall;
+    void* setWatchdogTimer;
+    // driver support services
+    void* connectController;
+    void* disconnectController;
+    // open and close protocol services
+    void* openProtocol;
+    void* closeProtocol;
+    void* openProtocolInformation;
+    // library services
+    void* protocolsPerHandle;
+    void* locateHandleBuffer;
+    void* locateProtocol;
+    void* installMultipleProtocolInterfaces;
+    void* uninstallMultipleProtocolInterfaces;
+    // 32-bit CRC services
+    void* calculateCrc32;
+    // misc services
+    void* copyMem;
+    void* setMem;
+    void* createEventEx;
+}
+
+struct EfiGuid
+{
+    uint data1;
+    ushort data2;
+    ushort data3;
+    ubyte[8] data4;
+}
+
+immutable EfiGuid EfiLoadedImageProtocolGuid = {
+    data1: 0x5B1B31A1, data2: 0x9562, data3: 0x11d2,
+    data4: [0x8e, 0x3f, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b]
+};
+
+struct EfiSimpleFileSystemProtocol
+{
+    ulong revision;
+    EfiStatus function(EfiSimpleFileSystemProtocol*, EfiFileProtocol**) openVolume;
+}
+
+immutable EfiGuid EfiSimpleFileSystemProtocolGuid = {
+    data1: 0x964e5b22, data2: 0x6459, data3: 0x11d2,
+    data4: [0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b]
+};
+
+struct EfiFileInfo
+{
+    ulong size;
+    ulong fileSize;
+    ulong physicalSize;
+    EfiTime createTime;
+    EfiTime lastAccessTime;
+    EfiTime modificationTime;
+    ulong attribute;
+    wchar[256] fileName;
+}
+
+struct EfiTime
+{
+    ushort year;
+    ubyte month;
+    ubyte day;
+    ubyte hour;
+    ubyte minute;
+    ubyte second;
+    ubyte pad1;
+    uint nanosecond;
+    ushort timeZone;
+    ubyte daylight;
+    ubyte pad2;
+}
+
+struct EfiFileProtocol
+{
+    ulong revision;
+    EfiStatus function(
+        EfiFileProtocol*, EfiFileProtocol**, wchar*, ulong, ulong
+    ) open;
+    void* close;
+    void* delete_;
+    void* read;
+    void* write;
+    void* getPosition;
+    void* setPosition;
+    EfiStatus function(
+        EfiFileProtocol*, EfiGuid*, uint*, void*
+    ) getInfo;
+    void* setInfo;
+    void* flush;
+    void* openEx;
+    void* readEx;
+    void* writeEx;
+    void* flushEx;
+}
+
+__gshared EfiGuid EfiFileInfoGuid = {
+    data1: 0x09576e92, data2: 0x6d3f, data3: 0x11d2,
+    data4: [0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b]
+};
